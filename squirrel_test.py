0f17b21270a63a866bf47ca6945f3d88aff6fbb2
wajeehrehman35 <81848256+wajeehrehman35@users.noreply.github.com>
2022-02-06 22:14:44 +0500

    Add files via upload

---
+++ squirrel_test.py
@@ -0,0 +1,58 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Feb  6 18:40:51 2022
+
+@author: wajee
+"""
+
+import pandas as pd
+
+gray_data = []
+cinnamon_data = []
+black_data = []
+
+get_file_path = "C:/Users/wajee/.spyder-py3/2018_Central_Park_Squirrel_Census_-_Squirrel_Data.csv"
+
+data = pd.read_csv(get_file_path)
+
+color_data = data["Primary Fur Color"]
+
+list_data = color_data.to_list()
+
+#print(list_data)
+
+for squirrel in list_data:
+    
+    if (squirrel == "Gray"):
+        gray_data.append(squirrel)
+    
+    if(squirrel == "Black"):
+        black_data.append(squirrel)
+    
+    if(squirrel == "Cinnamon"):
+        cinnamon_data.append(squirrel)
+        
+
+
+#print(gray_data)
+#print(black_data)
+#print(cinnamon_data)
+
+gray_count = gray_data.count("Gray")
+#print(gray_count)
+
+black_count = black_data.count("Black")
+#print(black_count)
+
+cinnamon_count = cinnamon_data.count("Cinnamon")
+#print(cinnamon_count)
+
+
+data_dict = {
+    "Fur Color" : ["Gray","Black","Cinnamon"],
+    "Count" : [gray_count,black_count,cinnamon_count]
+    }
+
+new_data = pd.DataFrame(data_dict)
+print(new_data)
+new_data.to_csv("squirrel_color.csv")
